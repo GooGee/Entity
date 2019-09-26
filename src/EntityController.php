3615d321a55620278dfdbe4f72ca4edc62ce725b
GooGee <mou-gj@live.com>
2019-09-26 22:37:02 +0800

    + getMySQLTable + getPGSQLTable

--- src/EntityController.php
+++ src/EntityController.php
@@ -46,4 +46,58 @@ class EntityController extends Controller
         return $this->send('');
     }
 
+    public function table()
+    {
+        $connection = config('database.default');
+        $path = "database.connections.{$connection}";
+        $driver = config("{$path}.driver");
+        $database = config("{$path}.database");
+        $prefix = config("{$path}.prefix");
+        $tables = [];
+        if ($driver == 'mysql') {
+            $tables = $this->getMySQLTable($database);
+        }
+        if ($driver == 'pgsql') {
+            $tables = $this->getPGSQLTable();
+        }
+        $data = [
+            'driver' => $driver,
+            'database' => $database,
+            'prefix' => $prefix,
+            'tables' => $tables,
+        ];
+        return $this->send($data);
+    }
+
+    private function getMySQLTable(string $database)
+    {
+        $tables = [];
+        $column = "Tables_in_{$database}";
+        $list = \DB::select('SHOW TABLES;');
+        foreach ($list as $item) {
+            $name = $item->$column;
+            $tables[] = [
+                'name' => $name,
+                'fields' => \DB::select("SHOW COLUMNS FROM {$name};"),
+                'indexes' => \DB::select("SHOW INDEX FROM {$name};"),
+            ];
+        }
+        return $tables;
+    }
+
+    private function getPGSQLTable()
+    {
+        $tables = [];
+        $list = \DB::select("SELECT * FROM information_schema.tables WHERE table_schema = 'public';");
+        foreach ($list as $item) {
+            $name = $item->table_name;
+            $tables[] = [
+                'name' => $name,
+                'fields' => \DB::select("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '{$name}';"),
+                'indexes' => \DB::select("SELECT * FROM pg_indexes WHERE tablename = '{$name}';"),
+            ];
+        }
+        return $tables;
+    }
+
 }
--- src/routes.php
+++ src/routes.php
@@ -4,4 +4,5 @@ Route::group(['namespace' => 'GooGee\\Entity'], function () {
     Route::get('entity', 'EntityController@load');
     Route::post('entity', 'EntityController@save');
     Route::post('entity/code', 'EntityController@deploy');
+    Route::get('entity/table', 'EntityController@table');
 });
