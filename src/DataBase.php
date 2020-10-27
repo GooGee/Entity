e1983e18a20b65dacaf3a996ea99230934424199
GooGee <mou-gj@live.com>
2020-10-27 14:39:22 +0800

    + DataBase.php

---
+++ src/DataBase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace GooGee\Entity;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+abstract class DataBase
+{
+
+    abstract protected function getSchema(string $database);
+
+    abstract protected function getTable(string $database);
+
+    static function getDB(MySQL $mySQL, PostgreSQL $postgreSQL)
+    {
+        $connection = config('database.default');
+        $path = "database.connections.{$connection}";
+        $driver = config("{$path}.driver");
+        $database = config("{$path}.database");
+        $prefix = config("{$path}.prefix");
+        $tables = [];
+        try {
+            if ($driver == 'mysql') {
+                $tables = $mySQL->getSchema($database);
+            }
+            if ($driver == 'pgsql') {
+                $tables = $postgreSQL->getSchema($database);
+            }
+        } catch (\Exception $exception) {
+            throw new HttpException(422, $exception->getMessage(), $exception);
+        }
+        $data = [
+            'driver' => $driver,
+            'database' => $database,
+            'prefix' => $prefix,
+            'tables' => $tables,
+        ];
+        return $data;
+    }
+}
