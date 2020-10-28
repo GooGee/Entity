40099efd5d339b7613eda1fe7f4409bd9ffbdba6
GooGee <mou-gj@live.com>
2020-10-28 21:09:25 +0800

    -+ no middleware in routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,9 @@
 <?php
 
-Route::group(['namespace' => 'GooGee\\Entity'], function () {
+Route::group(['namespace' => 'GooGee\\Entity', 'middleware' => []], function () {
     Route::get('entity', 'EntityController@load');
     Route::post('entity', 'EntityController@save');
     Route::post('entity/code', 'EntityController@deploy');
+    Route::post('entity/run', 'EntityController@run');
     Route::get('entity/table', 'EntityController@table');
 });
