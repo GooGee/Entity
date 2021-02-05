c87e3a28b8c0f4c754ab07ce9b4427b9c09f9c82
GooGee <mou-gj@live.com>
2021-02-05 11:02:01 +0800

    + cors in EntityController.php

--- src/EntityController.php
+++ src/EntityController.php
@@ -8,15 +8,35 @@ use Illuminate\Routing\Controller;
 class EntityController extends Controller
 {
     const Version = '2.4';
+    const Site = 'https://googee.github.io';
 
     private function send($data, $message = 'OK', $status = 200)
     {
+        $this->cors();
         return response()->json([
             'version' => self::Version,
             'status' => $status,
             'message' => $message,
             'data' => $data,
         ]);
+    }
+
+    private function cors()
+    {
+        header('Access-Control-Allow-Origin: ' . self::Site);
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');
+
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+            exit(0);
+        }
     }
 
     public function load()
